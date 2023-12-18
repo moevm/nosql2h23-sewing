7f4a40b99e725d13db4930d9f2ac9bdf399f114b
tvkoz <tv.kozlov.2002@gmail.com>
2023-12-18 04:07:06 +0300

    Add reg logic

--- backend/src/services/schemas/customer.py
+++ backend/src/services/schemas/customer.py
@@ -4,10 +4,10 @@ from pydantic import BaseModel, EmailStr, Field, constr
 class RestCustomerRegister(BaseModel):
     email: EmailStr = Field(description="New user email")
     password: constr(min_length=1, max_length=256) = Field(description="New user password")
-    TIN: constr(min_length=1, max_length=256) = Field(description="New user password")
-    name: constr(min_length=1, max_length=256) = Field(description="New user password")
-    contact_person: constr(min_length=1, max_length=256) = Field(description="New user password")
-    phone: constr(min_length=1, max_length=256) = Field(description="New user password")
+    TIN: constr(min_length=1, max_length=256) = Field(description="New user TIN")
+    name: constr(min_length=1, max_length=256) = Field(description="New user name")
+    contact_person: constr(min_length=1, max_length=256) = Field(description="New user contact_person")
+    phone: constr(min_length=1, max_length=256) = Field(description="New user phone")
 
 
 class RestCustomerLogin(BaseModel):
--- client/src/Registration.vue
+++ client/src/Registration.vue
@@ -9,52 +9,52 @@
               <div class="form-group">
                 <label for="login"><b>Логин/Email</b></label>
                 <input style="font-size: 12px" class="form-control" type="text" placeholder="Введите логин" name="login"
-                       id="login" required>
+                       id="login" ref="login" required>
               </div>
 
               <div class="form-group">
                 <label for="psw"><b>Пароль</b></label>
                 <input style="font-size: 12px" class="form-control" type="password" placeholder="Введите пароль"
-                       name="psw" id="psw" required>
+                       name="psw" id="psw" ref="psw" required>
               </div>
 
               <div class="form-group">
                 <label for="inn"><b>ИНН</b></label>
                 <input style="font-size: 12px" class="form-control" type="text" placeholder="Введите ИНН" name="inn"
-                       id="inn" required>
+                       id="inn" ref="inn" required>
               </div>
 
               <div class="form-group">
                 <label for="company-name"><b>Наименование компании</b></label>
                 <input style="font-size: 12px" class="form-control" type="text" placeholder="Введите имя компании"
                        name="company-name"
-                       id="company-name" required>
+                       id="company-name" ref="companyName" required>
               </div>
 
               <div class="form-group">
                 <label for="contact"><b>Контактное лицо (ФИО - должность)</b></label>
                 <input style="font-size: 12px" class="form-control" type="text" placeholder="Введите контактное лицо"
                        name="contact"
-                       id="contact" required>
+                       id="contact" ref="contact" required>
               </div>
 
               <div class="form-group">
                 <label for="phone"><b>Телефон для связи</b></label>
                 <input style="font-size: 12px" class="form-control" type="text" placeholder="+7 (XXX)-XXX-XXXX"
                        name="phone"
-                       id="phone" required>
+                       id="phone" ref="phone" required>
               </div>
 
               <div class="form-group">
                 <label class="radio-inline">
-                  <input type="radio" name="optradio">Покупатель
+                  <input type="radio" name="optradio" ref="customerRadio" checked>Покупатель
                 </label>
                 <label class="radio-inline" style="margin-left: 10px; margin-bottom: 6px;">
-                  <input type="radio" name="optradio">Поставщик
+                  <input type="radio" name="optradio" ref="companyRadio">Поставщик
                 </label>
               </div>
             </div>
-            <button type="submit" class="registerbtn">Регистрация</button>
+            <button type="submit" class="registerbtn" ref="submit">Регистрация</button>
           </div>
           <VueRecaptcha
               :sitekey="siteKey"
@@ -70,7 +70,33 @@
 
 <script>
 export default {
-  name: "RegistrationPage"
+  name: "RegistrationPage",
+  mounted() {
+    this.$refs.submit.addEventListener('click', () => {
+      let url = ""
+      if (this.$refs.customerRadio.checked) {
+        url = 'http://127.0.0.1:3000/api/customer/register'
+      } else {
+        url = 'http://127.0.0.1:3000/api/company/register'
+      }
+
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "email": String(this.$refs.login.value),
+          "password": String(this.$refs.psw.value),
+          "TIN": String(this.$refs.inn.value),
+          "name": String(this.$refs.companyName.value),
+          "contact_person": String(this.$refs.contact.value),
+          "phone": String(this.$refs.phone.value)
+        })
+      })
+    })
+  }
 }
 </script>
 
